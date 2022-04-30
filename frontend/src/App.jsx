e950091f2db38b4261e918db5c4f4fd27e9c7b71
Janis <janis.meister87@gmail.com>
2022-04-30 17:57:18 +0200

    INIT

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./css/App.scss";
+
+function App() {
+	return (
+		<div id="app">
+			<div id="content" className="app-container">
+				<h1>Hello World</h1>
+			</div>
+			<div id="sidebar" className="app-container">
+				<h1>Sidebar</h1>
+			</div>
+		</div>
+	);
+}
+
+export default App;
